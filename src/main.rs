ed1bf90275b25d590642e7d1af1cd13df5a89853
amachang <seijro@gmail.com>
2024-08-26 19:42:45 +0900

    allow to set crf manually

--- src/main.rs
+++ src/main.rs
@@ -13,8 +13,14 @@ enum Error {
     InvalidVideoPath(PathBuf),
     #[error("Failed to execute ab-av1 command: {0}")]
     AbAv1CommandFailed(ExitStatus),
+    #[error("Failed to execute force crf ffmpeg command: {0}")]
+    ForceCrfFfmpegCommandFailed(ExitStatus),
     #[error("Conflict between video path and encoding video path {1:?} for video {0:?}")]
     ConflictVideoEncoding(PathBuf, PathBuf),
+    #[error("Save path already exists for single encode: {0}")]
+    SingleEncodeSavePathAlreadyExists(PathBuf),
+    #[error("Single encode failed with invalid encoded file: {0}")]
+    SingleEncodeFailedWithInvalidEncodedFile(PathBuf, PathBuf),
 }
 
 #[derive(Deserialize, Serialize, Debug)]
@@ -49,6 +55,7 @@ struct Args {
 enum SubCommand {
     All(AllOpts),
     DebugSingle(DebugSingleOpts),
+    ForceCrfSingle(ForceCrfSingleOpts),
 }
 
 #[derive(Parser, Debug)]
@@ -63,6 +70,12 @@ struct DebugSingleOpts {
     target_vmaf: u8,
 }
 
+#[derive(Parser, Debug)]
+struct ForceCrfSingleOpts {
+    video_path: PathBuf,
+    crf: u8,
+}
+
 fn main() -> Result<()> {
     env_logger::init();
     let config = prepare_config()?;
@@ -72,6 +85,7 @@ fn main() -> Result<()> {
     match args.subcmd {
         SubCommand::All(opts) => run_all(opts, config)?,
         SubCommand::DebugSingle(opts) => run_debug_single_command(opts, config)?,
+        SubCommand::ForceCrfSingle(opts) => run_force_crf_single_command(opts, config)?,
     }
 
     Ok(())
@@ -91,15 +105,9 @@ fn run_all(opts: AllOpts, config: Config) -> Result<()> {
 
         // file_stem sometimes treats the last part of the file name as extension
         // so we impl the way below
-        let video_filename = video_path.file_name().ok_or(Error::InvalidVideoPath(video_path.clone()))?;
-        let mut iter = video_filename.as_encoded_bytes().rsplitn(1, |&b| b == b'.');
-        let video_slug = iter.next().ok_or(Error::InvalidVideoPath(video_path.clone()))?;
-        let video_slug = String::from_utf8_lossy(video_slug).to_string();
-        log::debug!("Video slug: {}", video_slug);
-
         let video_location_hash = hash_file_location(&video_path);
         let encoding_video_path = encodnig_video_dir.join(&video_location_hash).with_extension("mkv");
-        let save_path = save_dir.join(&video_slug).with_extension("mkv");
+        let save_path = encoded_file_save_path(&video_path, &config)?;
 
         if save_path.exists() {
             log::info!("Skipping video {:?} as it already exists in save directory", video_path);
@@ -110,7 +118,7 @@ fn run_all(opts: AllOpts, config: Config) -> Result<()> {
             return Err(anyhow!(Error::ConflictVideoEncoding(video_path, encoding_video_path)));
         }
 
-        println!("Encoding video: {}", video_filename.to_string_lossy());
+        println!("Encoding video: {}", video_path.display());
         match exec_ab_av1(&video_path, &encoding_video_path, opts.target_vmaf, false, &inherited_log_level, &config) {
             Ok(_) => log::info!("Encoding successful for {:?}", video_path),
             Err(e) => {
@@ -133,20 +141,9 @@ fn run_all(opts: AllOpts, config: Config) -> Result<()> {
             continue;
         }
 
-        println!("Saving video: {}", video_filename.to_string_lossy());
-        match fs::rename(&encoding_video_path, &save_path) {
-            Ok(_) => log::info!("Saved encoded video to {:?}", save_path),
-            Err(e) => {
-                match e.raw_os_error() {
-                    Some(libc::EXDEV) => {
-                        fs::copy(&encoding_video_path, &save_path)?;
-                        fs::remove_file(&encoding_video_path)?;
-                        log::info!("Saved encoded video (over device) to {:?}", save_path);
-                    },
-                    _ => return Err(e.into()),
-                }
-            }
-        }
+        println!("Saving video ...");
+        rename_file(&encoding_video_path, &save_path)?;
+        log::info!("Saved encoded video to {:?}", save_path);
     }
 
     Ok(())
@@ -159,6 +156,39 @@ fn run_debug_single_command(opts: DebugSingleOpts, config: Config) -> Result<()>
     log::info!("Output path: {:?}", output_path);
 
     exec_ab_av1(&opts.video_path, &output_path, opts.target_vmaf, true, "debug", &config)
+}
+
+fn run_force_crf_single_command(opts: ForceCrfSingleOpts, config: Config) -> Result<()> {
+    let save_dir = &config.save_dir;
+    let encodnig_video_dir = config.tmp_dir.join("encoding");
+    let video_path = &opts.video_path;
+    fs::create_dir_all(&save_dir)?;
+    fs::create_dir_all(&encodnig_video_dir)?;
+
+    let video_location_hash = hash_file_location(&video_path);
+    let encoding_video_path = encodnig_video_dir.join(&video_location_hash).with_extension("mkv");
+    let save_path = encoded_file_save_path(&opts.video_path, &config)?;
+
+    if save_path.exists() {
+        return Err(anyhow!(Error::SingleEncodeSavePathAlreadyExists(save_path)));
+    }
+
+    if encoding_video_path.exists() {
+        return Err(anyhow!(Error::ConflictVideoEncoding(video_path.clone(), encoding_video_path.clone())));
+    }
+
+    println!("Encoding video: {}", video_path.display());
+    exec_force_crf_ffmpeg(&opts.video_path, &encoding_video_path, opts.crf)?;
+
+    if encoding_video_path.exists() && !is_valid_video_file(&encoding_video_path)? {
+        fs::remove_file(&encoding_video_path)?;
+        return Err(anyhow!(Error::SingleEncodeFailedWithInvalidEncodedFile(video_path.clone(), encoding_video_path.clone())));
+    }
+
+    println!("Saving video ...");
+    rename_file(&encoding_video_path, &save_path)?;
+
+    Ok(())
 }
 
 fn exec_ab_av1(input_path: impl AsRef<Path>, output_path: impl AsRef<Path>, target_vmaf: u8, debug_intermediate_files: bool, log_level: impl AsRef<str>, config: &Config) -> Result<()> {
@@ -206,6 +236,40 @@ fn exec_ab_av1(input_path: impl AsRef<Path>, output_path: impl AsRef<Path>, targ
     }
 }
 
+// VMAF sometimes gives wrong results than human-sense score, for example, the reference video with VHD frame-vibrations, etc.
+// So, we support the feature just to set constant quality for ffmpeg
+fn exec_force_crf_ffmpeg(input_path: impl AsRef<Path>, output_path: impl AsRef<Path>, crf: u8) -> Result<()> {
+    let input_path = input_path.as_ref();
+    let output_path = output_path.as_ref();
+    let mut command = Command::new("ffmpeg");
+    command
+        .arg("-y")
+        .arg("-hwaccel").arg("cuda").arg("-hwaccel_output_format").arg("cuda")
+        .arg("-i").arg(input_path)
+        .arg("-c:v").arg("av1_nvenc")
+        .arg("-v:b").arg("0").arg("-rc").arg("vbr")
+        .arg("-preset").arg("p7")
+        .arg("-fps_mode").arg("passthrough")
+        .arg("-tune").arg("hq")
+        .arg("-temporal-aq").arg("1")
+        .arg("-rc-lookahead").arg("32")
+        .arg("-g").arg("300")
+        .arg("-cq").arg(crf.to_string())
+        .arg("-highbitdepth").arg("1")
+        .arg("-sn").arg("-dn")
+        .arg("-acodec").arg("aac")
+        .arg(output_path);
+
+    log::debug!("Command: {:?}", command);
+    let status = command.status()?;
+    log::debug!("Command status: {:?}", status);
+    if status.success() {
+       Ok(())
+    } else {
+        Err(anyhow!(Error::ForceCrfFfmpegCommandFailed(status)))
+    }
+}
+
 fn prepare_config() -> Result<Config> {
     let config_parent_dir = config_dir().ok_or(Error::ConfigDirNotFound)?;
     let config_dir = config_parent_dir.join(crate_name!());
@@ -224,6 +288,39 @@ fn prepare_config() -> Result<Config> {
     let config = config.try_deserialize::<Config>()?;
 
     Ok(config)
+}
+
+fn rename_file(from_path: impl AsRef<Path>, to_path: impl AsRef<Path>) -> Result<()> {
+    let from_path = from_path.as_ref();
+    let to_path = to_path.as_ref();
+    match fs::rename(from_path, to_path) {
+        Ok(_) => Ok(()),
+        Err(e) => {
+            match e.raw_os_error() {
+                Some(libc::EXDEV) => {
+                    fs::copy(from_path, to_path)?;
+                    fs::remove_file(from_path)?;
+                    Ok(())
+                },
+                _ => Err(e.into()),
+            }
+        }
+    }
+}
+
+fn encoded_file_save_path(video_path: impl AsRef<Path>, config: &Config) -> Result<PathBuf> {
+    let video_path = video_path.as_ref();
+    let save_dir = &config.save_dir;
+
+    // file_stem sometimes treats the last part of the file name as extension
+    // so we impl the way below
+    let video_filename = video_path.file_name().ok_or(Error::InvalidVideoPath(video_path.to_path_buf()))?;
+    let mut iter = video_filename.as_encoded_bytes().rsplitn(1, |&b| b == b'.');
+    let video_slug = iter.next().ok_or(Error::InvalidVideoPath(video_path.to_path_buf()))?;
+    let video_slug = String::from_utf8_lossy(video_slug).to_string();
+    let save_path = save_dir.join(&video_slug).with_extension("mkv");
+
+    Ok(save_path)
 }
 
 fn is_valid_video_file(video_path: impl AsRef<Path>) -> Result<bool> {
